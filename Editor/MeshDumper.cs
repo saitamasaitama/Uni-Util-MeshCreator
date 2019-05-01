c6f0def44d101b6e5d763ea4b9574666a1995008
unknown <s-yoshihara@saitama-tec.co.jp>
2019-05-01 20:46:49 +0900

    mesh dumper update

--- Editor/MeshDumper.cs
+++ Editor/MeshDumper.cs
@@ -3,52 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.Linq;
+using System.IO;
 
 [CustomEditor(typeof(MeshFilter))]
 public class MeshDumper : Editor
 {
-    public override void OnInspectorGUI(){
-        base.OnInspectorGUI();
+  delegate void Curry(string s);
+  public override void OnInspectorGUI()
+  {
+    base.OnInspectorGUI();
 
-        if(GUILayout.Button("Dump")){
-            MeshFilter mf=this.target as MeshFilter;
-            Debug.Log("Dump!");
-            MeshDump(mf.sharedMesh);
-        }
-    }
-    /*
-    public override void OnDrawGizmos() {
-
-    }
-    */
-    void Update()
+    if (GUILayout.Button("Dump"))
     {
-      if(Input.GetMouseButtonDown(0)){
-          Debug.Log(123);
-      }  //if(Input.)
+      MeshFilter mf = this.target as MeshFilter;
+      Debug.Log("Dump!");
+      MeshDump(mf.sharedMesh);
     }
+  }
+  /*
+  public override void OnDrawGizmos() {
 
-    public void MeshDump(Mesh old){
-        Mesh m=new Mesh();
+  }
+  */
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      Debug.Log(123);
+    }  //if(Input.)
+  }
 
-        m.SetVertices(old.vertices.ToList());
-        m.SetNormals(old.normals.ToList());
-        m.SetTriangles(old.triangles.ToList(),0);
+  public void MeshDump(Mesh old)
+  {
+    Mesh m = new Mesh();
 
-/*
-        foreach(Vector3 v in m.vertices){
-            Debug.Log(v);
-        }
-        Debug.Log("NORMAL");
-        foreach(Vector3 v in m.normals){
-            Debug.Log(v);
-        }
-        Debug.Log("TRIANGLES");
-        foreach(int v in m.triangles){
-            Debug.Log(v);
-        }
+    m.SetVertices(old.vertices.ToList());
+    m.SetNormals(old.normals.ToList());
+    m.SetTriangles(old.triangles.ToList(), 0);
+
+    string path = EditorUtility.SaveFilePanel("save mesh", "Assets/", "mesh.obj", "obj");
+
+    if (!string.IsNullOrEmpty(path))
+    {
+      //mesh2Objを呼び出す
+      using (StreamWriter writer = new StreamWriter(path, false, System.Text.Encoding.UTF8))
+      {
+        WriteObj(writer, old);
+      }
+    }
+  }
+
+  private void WriteObj(TextWriter o, Mesh m)
+  {
+    /*
+    g グループ名
+usemtl マテリアル名
+v x成分値 y成分値 z成分値
+v x成分値 y成分値 z成分値
+v x成分値 y成分値 z成分値
+…（省略）…
+vt x成分値 y成分値
+vt x成分値 y成分値
+vt x成分値 y成分値
+…（省略）…
+vn x成分値 y成分値 z成分値
+vn x成分値 y成分値 z成分値
+vn x成分値 y成分値 z成分値
+…（省略）…
+f 頂点座標値番号/テクスチャ座標値番号/頂点法線ベクトル番号 (多角形の頂点の数だけ続く）
+f 頂点座標値番号/テクスチャ座標値番号/頂点法線ベクトル番号 (多角形の頂点の数だけ続く）
+f 頂点座標値番号/テクスチャ座標値番号/頂点法線ベクトル番号 (多角形の頂点の数だけ続く
 */
-        AssetDatabase.CreateAsset(m,"Assets/shh.asset");
+    o.WriteLine($"g mesh");
 
+    m.vertices.ToArray().ToList().ForEach(v =>
+    {
+      o.WriteLine($"v {v.x:0.0000000} {v.y:0.0000000} {v.z:0.0000000}");
+    });
+
+    m.normals.ToArray().ToList().ForEach(v =>
+    {
+      o.WriteLine($"vn {v.x:0.0000000} {v.y:0.0000000} {v.z:0.0000000}");
+    });
+
+    for (int i = 0; i < m.triangles.Length / 3; i++)
+    {
+      o.WriteLine($"f {m.triangles[i * 3] + 1}//{i + 1} {m.triangles[i * 3 + 1] + 1}//{i + 1} {m.triangles[i * 3 + 2] + 1}//{i + 1}");
     }
+  }
+
 }
